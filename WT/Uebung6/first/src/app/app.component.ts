e247832bde825a4e8e8d65dfde8b4f1b0d0f30b2
lenaseverns <lenaseverns@gmail.com>
2023-03-24 14:41:35 +0100

    1414

--- WT/Uebung6/first/src/app/app.component.ts
+++ WT/Uebung6/first/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MyService } from '../app/shared/members.service';
+
 
 @Component({
-  selector: 'app-root',
+  selector: 'htw-main',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'first';
+export class MainComponent implements OnInit {
+  members: any ;
+
+  constructor(private myService: MyService) { }
+
+  ngOnInit(): void {
+    this.members = this.myService.getMembers();
+    console.log(this.members)
+  }
 }
--- WT/Uebung6/first/src/app/app.module.ts
+++ WT/Uebung6/first/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MainComponent } from '../app/app.component';
 import { HeaderComponent} from './header/header.component';
 /*import { NgbModule } from '@ng-bootstrap';*/
 
 @NgModule({
   declarations: [
-    AppComponent,
+    MainComponent,
     HeaderComponent
   ],
   imports: [
@@ -17,6 +17,6 @@ import { HeaderComponent} from './header/header.component';
     /*NgbModule*/
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainComponent]
 })
 export class AppModule { }
